c9edeebfb8b54005860f7c48a7a17728c3889d04
youngsamwei <w3wei@sina.com>
2018-12-07 19:37:50 +0800

    开始测试全是32位的设置

--- test_mysql_connector_cpp.cpp
+++ test_mysql_connector_cpp.cpp
@@ -25,54 +25,72 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
   (and mysql_connection.h). This will reduce your build time!
 */
 #include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "mysql_error.h"
+
 
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 using namespace std;
 
 int main(void)
 {
     cout << endl;
-    cout << "Running 'SELECT 'Hello World!' » AS _message'..." << endl;
+    cout << "Let's have MySQL count from 10 to 1..." << endl;
 
     try {
-        sql::Driver *driver;
-        sql::Connection *con;
-        sql::Statement *stmt;
-        sql::ResultSet *res;
+        sql::Driver *driver = NULL;
+        sql::Connection *con = NULL;
+        sql::Statement *stmt = NULL;
+        sql::ResultSet *res = NULL;
+        sql::PreparedStatement *pstmt = NULL;
 
         /* Create a connection */
         driver = get_driver_instance();
-        con = driver->connect("tcp://127.0.0.1:3306", "root", "123456");
+        con = driver->connect("localhost:3366", "root", "123456");
         /* Connect to the MySQL test database */
         con->setSchema("test");
 
         stmt = con->createStatement();
-        res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
-        while (res->next()) {
-            cout << "\t... MySQL replies: ";
-            /* Access column data by alias or column name */
-            cout << res->getString("_message") << endl;
-            cout << "\t... MySQL says it again: ";
-            /* Access column data by numeric offset, 1 is the first column */
-            cout << res->getString(1) << endl;
-        }
-        delete res;
+        stmt->execute("DROP TABLE IF EXISTS test");
+        stmt->execute("CREATE TABLE test(id INT)");
         delete stmt;
+
+        /* '?' is the supported placeholder syntax */
+        pstmt = con->prepareStatement("INSERT INTO test(id) VALUES (?)");
+        for (int i = 1; i <= 10; i++) {
+            pstmt->setInt(1, i);
+            pstmt->executeUpdate();
+        }
+        delete pstmt;
+
+        /* Select in ascending order */
+        pstmt = con->prepareStatement("SELECT id FROM test ORDER BY id ASC");
+        res = pstmt->executeQuery();
+
+        /* Fetch in reverse = descending order! */
+        res->afterLast();
+        while (res->previous())
+            cout << "\t... MySQL counts: " << res->getInt("id") << endl;
+        delete res;
+
+        delete pstmt;
         delete con;
 
     } catch (sql::SQLException &e) {
         cout << "# ERR: SQLException in " << __FILE__;
-        cout << "(" << __FUNCTION__ << ") on line "   << __LINE__ << endl;
+        cout << "(" << __FUNCTION__ << ") on line "  << __LINE__ << endl;
         cout << "# ERR: " << e.what();
         cout << " (MySQL error code: " << e.getErrorCode();
         cout << ", SQLState: " << e.getSQLState() << " )" << endl;
     }
 
     cout << endl;
+
 
     return EXIT_SUCCESS;
 }
